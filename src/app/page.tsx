a9852c977dad40b1f6c092f7aeaf9fe7e9204ff4
rafaela.bonacim <rafaela.bonacim@luizalabs.com>
2023-10-26 17:13:30 -0300

    feat: add initial html structure and header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,36 @@
-import Image from "next/image";
+import { Header } from "./components/header";
 
 export default function Home() {
 	return (
-		<main >
-			<Image
-				src="/vercel.svg"
-				alt="Vercel Logo"
-				width={100}
-				height={24}
-				priority
-			/>
+		<main>
+			<Header/>
+			<div className="experience">
+				<h3>Experience</h3>
+				<p>
+					2 years working as a Software Developer, in companys such as
+					Luizalabs.
+				</p>
+				<div className="experience-time"></div>
+				<div className="infos">
+					<h3>Languages</h3>
+					<div className="languages-info">
+						<span>🇺🇸 EN Fluent</span>
+						<span>🇧🇷 PT-BR Native Speaker</span>
+					</div>
+					<h3>Education</h3>
+					<div className="educational-info">
+						<span>🎓</span>
+						<span>
+							Production Engineering Bachelor s Degree - Universidade Federal de
+							Itajubá
+						</span>
+					</div>
+				</div>
+				<div className="buttons">
+					<div className="social"></div>
+					<button>contact me</button>
+				</div>
+			</div>
 		</main>
 	);
 }
