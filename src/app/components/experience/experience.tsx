9dda7c3ac394b3b46bc0bbf5dfb1e97a05e1176b
rafaela.bonacim <rafaela.bonacim@luizalabs.com>
2023-11-03 15:03:10 -0300

    feat: added experience

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,6 +1,7 @@
+import Image from "next/image";
 import { SectionTitle } from "../section-title/section-title";
 
-import "./experience.scss"
+import "./experience.scss";
 export function Experience() {
 	return (
 		<div className="experience">
@@ -8,7 +9,50 @@ export function Experience() {
 			<p>
 				2 years working as a Software Developer, in companys such as Luizalabs.
 			</p>
-			<div className="experience-time"></div>
+			<div className="experience-time">
+				<div className="experience-language">
+					<Image
+						src="/js.png"
+						alt="Javascript Logo"
+						width={40}
+						height={40}
+						priority
+					/>
+					<div className="experience-unit">
+						<div className="experience-measure measure-2">
+							<span>2 years</span>
+						</div>
+					</div>
+				</div>
+				<div className="experience-language">
+					<Image
+						src="/react.png"
+						alt="React Logo"
+						width={40}
+						height={40}
+						priority
+					/>
+					<div className="experience-unit">
+						<div className="experience-measure measure-2">
+							<span>2 years</span>
+						</div>
+					</div>
+				</div>
+				<div className="experience-language">
+					<Image
+						src="/ts.png"
+						alt="Typescript Logo"
+						width={40}
+						height={40}
+						priority
+					/>
+					<div className="experience-unit">
+						<div className="experience-measure measure-2">
+							<span>2 years</span>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
