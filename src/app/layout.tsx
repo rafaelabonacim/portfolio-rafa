3c9ad0d8d489dbb96c202bd2ecf4a3bb76e94531
rafaela.bonacim <rafaela.bonacim@luizalabs.com>
2023-11-08 23:01:50 -0300

    feat: added metadata description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,15 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.scss";
 
-const inter = Poppins({ subsets: ["latin"], weight: ["300","400", "500", "600"] });
+const inter = Poppins({
+	subsets: ["latin"],
+	weight: ["300", "400", "500", "600"],
+});
 
 export const metadata: Metadata = {
 	title: "Rafaela Bonacim",
-	description: "Rafaela Bonacim is a software engineer...",
+	description:
+		"Rafaela Bonacim is a software engineer passionate to developing amazing interfaces, with +5 years of experience in IT and +2 years as Front-End, who is constantly seeking for new challenges.",
 };
 
 export default function RootLayout({
