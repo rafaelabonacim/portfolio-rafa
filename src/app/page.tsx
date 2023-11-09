8ffb08804278c953da96faa8e446540bf8869c6b
rafaela.bonacim <rafaela.bonacim@luizalabs.com>
2023-11-08 22:30:37 -0300

    refactor: removed line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import { Experience } from "./components/experience/experience";
 import { Header } from "./components/header/header";
 import { Info } from "./components/information/information";
 import "./styles/home.scss";
-import { EmailIcon } from "./components/icons/email-icon";
-import { SocialBtns } from "./components/social-btns/social-btns";
 import { Projects } from "./components/projects/projects";
 
 export default function Home() {
